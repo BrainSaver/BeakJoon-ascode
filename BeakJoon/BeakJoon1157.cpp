262df5b1b2080e9b4ec20c596313054da44cdaf6
Rokking <sk9910064@gmail.com>
2024-03-25 22:07:53 +0900

    드디어... C/C++을...(최종)

---
+++ BeakJoon/BeakJoon1157.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a=0;
+    cin >> a;
+    if(a==100){
+    cout << "Hello World\n";
+    }
+    cout << "Done.\n";
+}
--- C++Language.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#pragma warning(disable : 4996)
-using namespace std;
-
-int main() {
-	int a=0;
-	cin >> a;
-	cout << a*10;
-}
