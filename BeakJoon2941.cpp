cd12821d9f0b3133429a874f771f5e6338d42034
Rokking <sk9910064@gmail.com>
2024-03-26 12:47:17 +0900

    백준 2941 클리어

--- BeakJoon2941.cpp
+++ BeakJoon2941.cpp
@@ -6,5 +6,17 @@ int main() {
 ios_base :: sync_with_stdio(false); 
 cin.tie(NULL); 
 cout.tie(NULL);
+    int star=0;
 
+        cin >> star;
+    for(int a=0; a<star; a++){
+        for(int c=0; c<a; c++){
+            cout<<" ";
+        }
+        for(int b=0; b<star-((a-1)*2); b++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
 }
