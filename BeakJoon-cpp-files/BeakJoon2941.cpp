c4bc0fe954e63b34951a09d6a1440255fdbf6217
Rokking <sk9910064@gmail.com>
2024-03-29 01:41:58 +0900

    BeakJoon2941 클리어,,,, 드디어

--- BeakJoon-cpp-files/BeakJoon2941.cpp
+++ BeakJoon-cpp-files/BeakJoon2941.cpp
@@ -1,51 +1,30 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #pragma warning(disable : 4996)
 using namespace std;
-/*
-č	c=
-ć	c-
-dž	dz=
-đ	d-
-lj	lj
-nj	nj
-š	s=
-ž	z=
-*/
 
-int solution(string my_string, string target) {
-    
-    if (my_string.find(target) != string::npos && my_string != "dz=") return 1;
-    else if(my_string == "dz=") return 2;
-    else return 0;
+string find_and_replace(string my_string, string target) {
+    size_t found = my_string.find(target);
+    while (found != string::npos) {
+        my_string.replace(found, target.size(), "a");
+        found = my_string.find(target, found+1);
+    }
+    return my_string;
 }
 
 int main() {
-ios_base :: sync_with_stdio(false); 
-cin.tie(NULL); 
-cout.tie(NULL);
-    char alpha[8][8]={{"c="}, {"c-"}, {"dz="}, {"d-"}, {"lj"}, {"nj"}, {"s="}, {"z="}};
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string alpha[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
     string word;
 
-    /*
-    ljes=njak
-    포함된 알파벳: lj, nj, s=
-    단어 길이: 9, 9-3 = 6
+    cin >> word;
+    for (int a = 0; a < 8; a++) {
+        word = find_and_replace(word, alpha[a]);
+    }
+    cout << word.size();
 
-    ddz=z=
-    dz=: 2, z= : 1
-    단어 길이: 6 - 3 = 3
-    
-    nljj
-    lj : 1
-    단어 길이 : 4 - 1 = 3
-    */
-   cin >> word;
-   int num=word.size();
-   for(int a=0; a<word.size(); a++){
-    if (word.find(alpha[a]) != string::npos && word != "dz=") num+=1;
-    else if(word == "dz=") num+=2;
-
-   }
-
+    return 0;
 }
